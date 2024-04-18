1941fcdc5a728068204a432f190cf001ba36e227
flabasha <kridid@outlook.com>
2024-04-18 12:58:25 +0200

    Daddyissues

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Sjekk vårt utvalg av bøker slik at du kan få en kort vei til nyttig spesialist.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Utvalg</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="/boker">Utvalg</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Lær mer</a>
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -12,7 +12,7 @@ export default function Zigzag() {
 
             {/* Section header */}
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-              <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
+              <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Nå mål som betyr noe...</div>
               <h1 className="h2 mb-4">Ett produkt, ubegrenset kunnskap</h1>
               <p className="text-xl text-gray-400">Ikke bare gi magen din pumping, pump også hjernen</p>
             </div>
