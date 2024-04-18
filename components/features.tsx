b01cf85adc6041901c0808603bcc8986c8727163
flabasha <kridid@outlook.com>
2024-04-18 12:39:29 +0200

    More commitment issues

---
+++ components/features.tsx
@@ -0,0 +1,55 @@
+export default function Features() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">De fleste studenter forstår ikke deres studieflyt</h2>
+            <p className="text-xl text-gray-400">Det er derfor vi har gjennom timer med blod, svette, tårer og mange kilo koffein, satt sammen denne løsningen slik at du kan få tilgang til det du trenger for å fungere i samfunnet.</p>
+          </div>
+
+          {/* Items */}
+          <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg>
+              <h4 className="h4 mb-2">Øyeblikkelig tilgang</h4>
+              <p className="text-lg text-gray-400 text-center">Ved å benytte denne tjenesten får du øyeblikkelig tilgang til det du trenger, når du trenger det. Ingen lange køer på biblioteket..Er det egentlig det lenger?</p>
+            </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
+                <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
+              </svg>
+              <h4 className="h4 mb-2">Lei bøker? Vi også, gå digitalt</h4>
+              <p className="text-lg text-gray-400 text-center">Kan vi friste med en kopp kaffe til lesingen? Kom på campus og legg kortet ditt på automaten. Kun 5 kr i dag!</p>
+            </div>
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg>
+              <h4 className="h4 mb-2">Vi har også et "voksenbibliotek"</h4>
+              <p className="text-lg text-gray-400 text-center">Hvorfor ikke slappe av litt mellom slagene med et "voksenbibliotek"?</p>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
